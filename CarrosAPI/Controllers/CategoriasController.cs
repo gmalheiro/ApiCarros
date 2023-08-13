e514e513defc0b579c7625460b94e5a322bac113
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-13 14:14:23 -0300

    Created CategoriasController

---
+++ CarrosAPI/Controllers/CategoriasController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarrosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly IConfiguration? _configuration;
+
+        public CategoriasController(IConfiguration configuration)
+        {
+                _configuration = configuration;
+        }
+
+    }
+}
