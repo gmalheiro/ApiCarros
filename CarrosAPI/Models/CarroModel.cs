3fb901efa46a45a715fe12da340b911e52fee6bf
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-14 18:53:18 -0300

    Added CarroModel

---
+++ CarrosAPI/Models/CarroModel.cs
@@ -0,0 +1,20 @@
+﻿namespace CarrosAPI.Models
+{
+    public class CarroModel
+    {
+
+        public int CarroId { get; set; }
+        public string? Modelo { get; set; }
+        public string? Descricao { get; set; }
+        public int CategoriaId { get; set; }
+
+        public CarroModel(int carroId, string modelo, string descricao, int categoriaId)
+        {
+            CarroId = carroId;
+            Modelo = modelo;
+            Descricao = descricao;
+            CategoriaId = categoriaId;
+        }
+
+    }
+}
